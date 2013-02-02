732d4391340b934c503308c6b42ae8cf8fd9a83c
john-lay <john.lay@hotmail.co.uk>
2013-02-02 18:58:18 +0000

    Basic animation and game timer

--- the-lost-eye-of-thundera/Form1.Designer.cs
+++ the-lost-eye-of-thundera/Form1.Designer.cs
@@ -31,6 +31,8 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnStart = new System.Windows.Forms.Button();
+            this.labelTimer = new System.Windows.Forms.Label();
+            this.labelSecondTimer = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,11 +55,31 @@
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
+            // labelTimer
+            // 
+            this.labelTimer.AutoSize = true;
+            this.labelTimer.Location = new System.Drawing.Point(322, 15);
+            this.labelTimer.Name = "labelTimer";
+            this.labelTimer.Size = new System.Drawing.Size(36, 13);
+            this.labelTimer.TabIndex = 2;
+            this.labelTimer.Text = "Timer:";
+            // 
+            // labelSecondTimer
+            // 
+            this.labelSecondTimer.AutoSize = true;
+            this.labelSecondTimer.Location = new System.Drawing.Point(322, 41);
+            this.labelSecondTimer.Name = "labelSecondTimer";
+            this.labelSecondTimer.Size = new System.Drawing.Size(36, 13);
+            this.labelSecondTimer.TabIndex = 3;
+            this.labelSecondTimer.Text = "Timer:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(632, 446);
+            this.Controls.Add(this.labelSecondTimer);
+            this.Controls.Add(this.labelTimer);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.pictureBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -65,6 +87,7 @@
             this.Text = "Thundercats: The lost eye of Thundera";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -72,6 +95,8 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label labelTimer;
+        private System.Windows.Forms.Label labelSecondTimer;
     }
 }
 
--- the-lost-eye-of-thundera/Form1.cs
+++ the-lost-eye-of-thundera/Form1.cs
@@ -12,18 +12,77 @@ namespace the_lost_eye_of_thundera
 {
     public partial class Form1 : Form
     {
+        Timer GameTimer;
+        Graphics gameCanvas;
+        Liono liono;
+        static bool gameStart = false;
+        static bool pause = false;
+        static int tenth = 0;
+        static int second = 0;
+        static int animationStep = 0;
+
         public Form1()
         {
             InitializeComponent();
+            //set up the game timer
+            GameTimer = new Timer();
+            GameTimer.Interval = 100;
+            GameTimer.Tick += new EventHandler(GameTimer_Tick);
+            //create the graphics object to draw with            
+            gameCanvas = pictureBox1.CreateGraphics();
+            //initialise sprite
+            liono = new Liono();
+
+            //start the game
+            if (gameStart == true) {                
+                GameTimer.Start();
+            }
         }
 
         private void btnStart_Click(object sender, EventArgs e)
         {
-            //create the graphics object to draw with
-            Graphics paper;
-            paper = pictureBox1.CreateGraphics();
-            Liono liono = new Liono();
-            paper.DrawImage(liono.walk[0], 10, 100);
+            //initialise the game for the first time
+            if (gameStart == false) {
+                gameStart = true;
+            }
+            //pause/resume game
+            if (pause == true) {
+                GameTimer.Stop();
+                btnStart.Text = "Resume";
+                pause = false;
+            }
+            else {
+                GameTimer.Start();
+                btnStart.Text = "Pause";
+                pause = true;
+            }            
+        }
+
+        void GameTimer_Tick(object sender, EventArgs e)
+        {
+            //Draw();
+            //gameCanvas.DrawImage(liono.walk[0], 10, 100);
+            
+            //timers
+            labelTimer.Text = "Timer: " + tenth;
+            tenth++;
+            labelSecondTimer.Text = "Timer: " + second;
+            if (tenth % 10 == 0) {
+                second++;
+            }
+            //draw walk cycle - 8 frames
+            if (animationStep == 8)
+            {
+                animationStep = 0;
+            }
+            else
+            {
+                animationStep++;
+            }
+            //clear the canvas
+            gameCanvas.Clear(SystemColors.Control);
+            //draw updated frame
+            gameCanvas.DrawImage(liono.walk[animationStep], 10, 100);
         }
     }
 }
